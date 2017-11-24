c8b0c498f5ecb74329e24717f3897dbb5a35566d
TejaswiKasarla <tejaswi.kasarla1@gmail.com>
2017-11-24 23:38:13 +0530

    removed folders

--- envs/mujoco_safe/walker2d_env_safe.py
+++
@@ -1,7 +0,0 @@
-from rllab.core.serializable import Serializable
-from rllab.envs.mujoco.walker2d_env import Walker2DEnv
-from sandbox.cpo.envs.mujoco_safe.mujoco_env_safe import SafeMujocoEnv
-
-class SafeWalker2DEnv(SafeMujocoEnv, Serializable):
-
-    MODEL_CLASS = Walker2DEnv
